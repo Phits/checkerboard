79aaa882801bafdd32fed61e4708083ff87c8bf1
Phits <john@fitz.com>
2023-03-31 14:06:19 -0400

    Start Play

--- script.js
+++ script.js
@@ -45,40 +45,76 @@ function createTiles() {
     let leftEven = 415;
     let topOdd = 133;
     let topEven = 133;
+    let topYellow = 147;
+    let leftYellow = 352;
+    let topOrange = 533;
+    let leftOrange = 390.5;
 
     for (let i = 0; i < numberOfTiles; i++) {
-        leftOdd = leftOdd + 77;
-        leftEven = leftEven + 77;
+        let offset = 77;
+        leftOdd = leftOdd + offset;
+        leftEven = leftEven + offset;
+        leftYellow = leftYellow + offset/2;
+        leftOrange = leftOrange + offset/2;
 
-        if (leftOdd > 877) {;
-            topOdd = topOdd + 77;
+        if (leftOdd > 877) {
+            topOdd = topOdd + offset;
             leftOdd = 415;
         }
 
-        if (leftEven > 877) {;
-            topEven = topEven + 77
+        if (leftEven > 877) {
+            topEven = topEven + offset;
             leftEven = 415;
         }
 
-        console.log(i)
+        if (leftYellow > 954 && topYellow < 224) {
+            topYellow = topYellow + offset;
+            leftYellow = 429;
+        }
+
+        if (leftOrange > 891) {
+            topOrange = topOrange + offset;
+            leftOrange = 390.5;
+        }
+
+
+            // topOrange = topOrange + offset;
+            // leftYellow = 429;
+
+
+
         if (i % 2 === 0 && i !== 48) {
 
+            // Squares
             let obj = document.createElement('div');
-            obj.className = `white-${1}`;
+            obj.className = `white-${i}`;
             obj.style.cssText = `position: absolute; top: ${topEven}px; left: ${leftEven}px; width: 77px; height: 77px; background: #000000 0% 0% no-repeat padding-box; opacity: 1;`;
 
             document.getElementById("tilesEven").appendChild(obj);
 
+            // Checker
+            if (i < 28) {
+                let checker = document.createElement('div');
+                checker.className = `orange-${i}`;
+                checker.style.cssText = `position: absolute; top: ${topOrange}px; left: ${leftOrange}px; width: 50px; height: 50px; background: #F67B5B 0 0 no-repeat padding-box; opacity: 1; border-radius: 50%;`;
+
+                document.getElementById("orangeCheckers").appendChild(checker);
+            }
         } else {
             let obj = document.createElement('div');
-            obj.className = `blue-${1}`;
+            obj.className = `blue-${i}`;
             obj.style.cssText = `position: absolute; top: ${topOdd}px;left: ${leftOdd}px;width: 77px;height: 77px;background: #0066A1 0 0 no-repeat padding-box;opacity: 1;`;
 
             document.getElementById("tilesOdd").appendChild(obj);
+
+            if (i < 28) {
+                let checker = document.createElement('div');
+                checker.className = `yellow-${i}`;
+                checker.style.cssText = `position: absolute; top: ${topYellow}px; left: ${leftYellow}px; width: 50px; height: 50px; background: #F8EB75 0 0 no-repeat padding-box;border-radius: 50%; opacity: 1;`;
+
+                document.getElementById("yellowCheckers").appendChild(checker);
+            }
         }
-
-
-
     }
 
 }
