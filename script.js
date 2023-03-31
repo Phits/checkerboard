d3815b7d440a939ac57a34ea3e44f45042d1503d
Phits <john@fitz.com>
2023-03-31 12:35:05 -0400

    Tiles working

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 'use strict';
 
-$( function() {
-    $( "#datepicker" ).datepicker({
-        dateFormat: "dd-mm-yy",
-    });
-} );
+$(document).ready(function() {
+
+    $( function() {
+        $( "#datepicker" ).datepicker({
+            dateFormat: "dd-mm-yy",
+        });
+    } );
+
+    createTiles();
+
+}); /* End document ready */
 
 function hideDefault() {
-    console.log('Hide');
     document.getElementById("datepicker-calendar").classList.add("hide");
-
-    // if (x.style.display === "none") {
-    //     x.style.display = "block";
-    // } else {
-    //     x.style.display = "none";
-    // }
 }
 
-function myFunction() {
+function toggleDrop() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
@@ -39,10 +38,50 @@ function filterFunction() {
     }
 }
 
-$(document).ready(function() {
+function createTiles() {
 
-    // let defaultDate = 'Calendar (dd-mm-yyy)';
-    // $("#datepicker").val(defaultDate);
+    const numberOfTiles = 50;
+    let leftOdd = 261;
+    let leftEven = 415;
+    let topOdd = 133;
+    let topEven = 133;
 
-}); /* End document ready */
+    for (let i = 0; i < numberOfTiles; i++) {
+        leftOdd = leftOdd + 77;
+        leftEven = leftEven + 77;
+
+        if (leftOdd > 877) {;
+            topOdd = topOdd + 77;
+            leftOdd = 415;
+        }
+
+        if (leftEven > 877) {;
+            topEven = topEven + 77
+            leftEven = 415;
+        }
+
+        console.log(i)
+        if (i % 2 === 0 && i !== 48) {
+
+            let obj = document.createElement('div');
+            obj.className = `white-${1}`;
+            obj.style.cssText = `position: absolute; top: ${topEven}px; left: ${leftEven}px; width: 77px; height: 77px; background: #000000 0% 0% no-repeat padding-box; opacity: 1;`;
+
+            document.getElementById("tilesEven").appendChild(obj);
+
+        } else {
+            let obj = document.createElement('div');
+            obj.className = `blue-${1}`;
+            obj.style.cssText = `position: absolute; top: ${topOdd}px;left: ${leftOdd}px;width: 77px;height: 77px;background: #0066A1 0 0 no-repeat padding-box;opacity: 1;`;
+
+            document.getElementById("tilesOdd").appendChild(obj);
+        }
+
+
+
+    }
+
+}
+
+
 
